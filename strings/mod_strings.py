f8b21b042c8c10facfc28752b56480016e5f0a4d
Caio Dias <caiodevgit@gmail.com>
2026-02-04 17:38:17 -0200

    Continuei a estudar Strings no novo computador

--- strings/mod_strings.py
+++ strings/mod_strings.py
@@ -17,3 +17,33 @@ print("--------------F strings: ----------------\n")
 print(f"{s:^20}")
 print(f"{s:.<20}")
 print(f"{s:.>20}")
+
+string = "um tigre, dois tigres, três tigres"
+linhas = "uma linha\nduas linhas\ntrês linhas"
+trocar = "um doido, dois doidos e um doido"
+
+verificaTudoNum = "123456"
+verificaAlpha = "oi"
+verificaDigito = "537124958712"
+
+emaiusculo = "AAAAAA"
+eminusculo = "abcdeeeee"
+
+estaEmBranco = "      "
+
+print(string.split(","))
+print(linhas.splitlines())
+print(trocar.replace("doido", "gato", 2))
+
+print(verificaTudoNum.isalnum())
+print(verificaAlpha.isalpha())
+print(verificaDigito.isdigit())
+
+print("\n")
+
+print(emaiusculo.isupper())
+print(eminusculo.islower())
+
+print("\n")
+
+print(estaEmBranco.isspace())
